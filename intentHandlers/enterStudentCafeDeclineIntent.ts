1534e81be18948fbab1a7bce4ca2a7d2a4b67385
Escape TU Dev Team <escapetu_dev@h2986705.stratoserver.net>
2023-02-01 00:14:45 +0100

    wrong txt linked

--- intentHandlers/enterStudentCafeDeclineIntent.ts
+++ intentHandlers/enterStudentCafeDeclineIntent.ts
@@ -6,7 +6,7 @@ import getText from "../getText";
 export default function EnterStudentCafeDeclineIntent(gameSession: GameSession, query: QueryResult): DfResponse | undefined {
    const responseBuilder = new ResponseBuilder();
     
-   responseBuilder.addMessage(getText("game_over_enter"))
+   responseBuilder.addMessage(getText("game_over_entry"))
    gameSession.helpText = "You just declined to enter the student cafe and the zombies got to you. You lost the game. You can restart the game."
    
    return responseBuilder.build();
