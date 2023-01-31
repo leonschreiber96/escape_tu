227f8599e4cd3e48ee1ff7e0bee700e9af8b4617
Escape TU Dev Team <escapetu_dev@h2986705.stratoserver.net>
2023-02-01 00:04:08 +0100

    trying to fix math building logic mistake

--- intentHandlers/talkPorterIntent.ts
+++ intentHandlers/talkPorterIntent.ts
@@ -5,9 +5,12 @@ import getText from "../getText";
 
 export default function TalkPorterIntent(gameSession: GameSession, query: QueryResult): DfResponse | undefined {
    const responseBuilder = new ResponseBuilder();
-
-   responseBuilder.addMessage(getText("main_building_doorkeeper"))
-   gameSession.helpText = "The porter just told you that you'll probably find the cheat sheets in the math building. Or maybe in the cafeteria...?"
-
+   if(gameSession.hasCheatSheet == false){
+      responseBuilder.addMessage(getText("main_building_doorkeeper"))
+      gameSession.helpText = "The porter just told you that you'll probably find the cheat sheets in the math building. Or maybe in the cafeteria...?"
+   } else {
+      responseBuilder.addMessage(getText("porter_with_cheat_sheet"))
+      gameSession.helpText = "The porter just told you that you'll probably find the key to the library in the student cafe. Or maybe in the cafeteria...?"
+   }
    return responseBuilder.build();
 }
